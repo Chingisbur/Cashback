e164433e0dcce3336f1069e92d68df0c96a985a0
Chingis <chingisbur@gmail.com>
2024-02-20 16:00:22 +0300

    Junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,43 @@
-import static org.testng.Assert.*;
+package ru.netology.service;
+
+import static org.junit.Assert.*;
+
 public class CashbackHackServiceTest {
-    
+    @org.junit.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testRemainOver1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 700;
+        int actual = service.remain(amount);
+        int expected = 300;
+        assertEquals(actual, expected);
+
+    }
+
+    @org.junit.Test
+    public void testRemainZero() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+
+    }
 }
